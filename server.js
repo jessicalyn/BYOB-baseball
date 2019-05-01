181ca5a3b95c482f2ebdce2f368607d08816b7e9
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2019-05-01 11:50:26 -0600

    install express, setup GET all divisions endpoint

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+const app = express()
+const port = 3000
+app.use(express.json())
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+app.get('/api/v1/divisions', (request, response) => {
+  database('divisions').select()
+    .then((divisions) => {
+      response.status(200).json(divisions)
+    })
+    .catch((error) => {
+      response.status(500).json({ error })
+    })
+})
+
+app.get('/api/v1/teams', (request, response) => {
+  database('teams').select()
+    .then((teams) => {
+      response.status(200).json(divisions)
+    })
+    .catch((error) => {
+      response.status(500).json({ error })
+    })
+})
