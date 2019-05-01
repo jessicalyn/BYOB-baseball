b58df3ddefc162083ae098ab00f59aef7a78fba2
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2019-05-01 10:17:55 -0600

    setup knex

---
+++ knexfile.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/baseball',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  },
+
+};
