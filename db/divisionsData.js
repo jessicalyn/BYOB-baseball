4a740f272ad6448e5a9407f07b3e1f33b1e3dea8
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2019-05-01 10:18:15 -0600

    add divisionsData file

---
+++ db/divisionsData.js
@@ -0,0 +1,33 @@
+let divisionsData = [{
+  name: 'AL East',
+  league: 'American League',
+  teams: [{
+    name: 'Oakland Athletics', 
+    stadium_name: 'Colliseum', 
+    website: 'www.as.com',
+    division_name: "AL West"
+  }]
+},
+{
+  name: 'AL Central',
+  league: 'American League'
+},
+{
+  name: 'AL West',
+  league: 'American League'
+},
+{
+  name: 'NL East',
+  league: 'National League'
+},
+{
+  name: 'NL Central',
+  league: 'National League'
+},
+{
+  name: 'NL West',
+  league: 'National League'
+}]
+
+
+module.exports = divisionsData
