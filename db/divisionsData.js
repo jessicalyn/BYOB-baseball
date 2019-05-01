8711c191dac4dac373d70517d57a6dff144b6fb9
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2019-05-01 11:15:08 -0600

    complete divisionsData

--- db/divisionsData.js
+++ db/divisionsData.js
@@ -1,33 +1,218 @@
 let divisionsData = [{
   name: 'AL East',
   league: 'American League',
-  teams: [{
-    name: 'Oakland Athletics', 
-    stadium_name: 'Colliseum', 
-    website: 'www.as.com',
-    division_name: "AL West"
-  }]
+  teams: [
+    {
+      name: 'Baltimore Orioles', 
+      stadium_name: 'Oriole Park at Camden Yards', 
+      website: 'www.orioles.com',
+      division_name: "AL East"
+    },
+    {
+      name: 'Boston Red Sox', 
+      stadium_name: 'Fenway Park', 
+      website: 'www.redsox.com',
+      division_name: "AL East"
+    },
+    {
+      name: 'New York Yankees', 
+      stadium_name: 'Yankees Stadium', 
+      website: 'www.yankees.com',
+      division_name: "AL East"
+    },
+    {
+      name: 'Tampa Bay Rays', 
+      stadium_name: 'Tropicana Field', 
+      website: 'www.raysbaseball.com',
+      division_name: "AL East"
+    },
+    {
+      name: 'Toronto Blue Jays', 
+      stadium_name: 'Rogers Centre', 
+      website: 'www.bluejays.com',
+      division_name: "AL East"
+    }
+  ]
 },
 {
   name: 'AL Central',
-  league: 'American League'
+  league: 'American League',
+  teams: [
+    {
+      name: 'Chicago White Sox', 
+      stadium_name: 'Guaranteed Rate Field', 
+      website: 'www.whitesox.com',
+      division_name: "AL Central"
+    },
+    {
+      name: 'Cleveland Indians', 
+      stadium_name: 'Progressive Field', 
+      website: 'www.indians.com',
+      division_name: "AL Central"
+    },
+    {
+      name: 'Detroit Tigers', 
+      stadium_name: 'Comerica Park', 
+      website: 'www.tigers.com',
+      division_name: "AL Central"
+    },
+    {
+      name: 'Kansas City Royals', 
+      stadium_name: 'Kauffman Stadium', 
+      website: 'www.royals.com',
+      division_name: "AL Central"
+    },
+    {
+      name: 'Minnesota Twins', 
+      stadium_name: 'Target Field', 
+      website: 'www.twinsbaseball.com',
+      division_name: "AL Central"
+    }
+  ]
 },
 {
   name: 'AL West',
-  league: 'American League'
+  league: 'American League',
+  teams: [
+    {
+      name: 'Houston Astros', 
+      stadium_name: 'Minute Maid Park', 
+      website: 'www.astros.com',
+      division_name: "AL West"
+    },
+    {
+      name: 'Los Angeles Angels', 
+      stadium_name: 'Angel Stadium', 
+      website: 'www.angels.com',
+      division_name: "AL West"
+    },
+    {
+      name: 'Oakland Athletics', 
+      stadium_name: 'Oakland Coliseum', 
+      website: 'www.athletics.com',
+      division_name: "AL West"
+    },
+    {
+      name: 'Seattle Mariners', 
+      stadium_name: 'T-Mobile Park', 
+      website: 'www.mariners.com',
+      division_name: "AL West"
+    },
+    {
+      name: 'Texas Rangers', 
+      stadium_name: 'Globe Life Park in Arlington', 
+      website: 'www.texasrangers.com',
+      division_name: "AL West"
+    }
+  ]
 },
 {
   name: 'NL East',
-  league: 'National League'
+  league: 'National League',
+  teams: [
+    {
+      name: 'Atlanta Braves', 
+      stadium_name: 'SunTrust Park', 
+      website: 'www.braves.com',
+      division_name: "NL East"
+    },
+    {
+      name: 'Miami Marlins', 
+      stadium_name: 'Marlins Park', 
+      website: 'www.marlins.com',
+      division_name: "NL East"
+    },
+    {
+      name: 'New York Mets', 
+      stadium_name: 'Citi Field', 
+      website: 'www.mets.com',
+      division_name: "NL East"
+    },
+    {
+      name: 'Philadelphia Phillies', 
+      stadium_name: 'Citizens Bank Park', 
+      website: 'www.phillies.com',
+      division_name: "NL East"
+    },
+    {
+      name: 'Washington Nationals', 
+      stadium_name: 'Nationals Park', 
+      website: 'www.nationals.com',
+      division_name: "NL East"
+    }
+  ]
 },
 {
   name: 'NL Central',
-  league: 'National League'
+  league: 'National League',
+  teams: [
+    {
+      name: 'Chicago Cubs', 
+      stadium_name: 'Wrigley Field', 
+      website: 'www.cubs.com',
+      division_name: "NL Central"
+    },
+    {
+      name: 'Cincinnati Reds', 
+      stadium_name: 'Great American Ball Park', 
+      website: 'www.reds.com',
+      division_name: "NL Central"
+    },
+    {
+      name: 'Milwaukee Brewers', 
+      stadium_name: 'Miller Park', 
+      website: 'www.brewers.com',
+      division_name: "NL Central"
+    },
+    {
+      name: 'Pittsburgh Pirates', 
+      stadium_name: 'PNC Park', 
+      website: 'www.pirates.com',
+      division_name: "NL Central"
+    },
+    {
+      name: 'St. Louis Cardinals', 
+      stadium_name: 'Busch Stadium', 
+      website: 'www.cardinals.com',
+      division_name: "NL Central"
+    }
+  ]
 },
 {
   name: 'NL West',
-  league: 'National League'
+  league: 'National League',
+  teams: [
+    {
+      name: 'Arizona Diamondbacks', 
+      stadium_name: 'Chase Field', 
+      website: 'www.dbacks.com',
+      division_name: "NL West"
+    },
+    {
+      name: 'Colorado Rockies', 
+      stadium_name: 'Coors Field', 
+      website: 'www.rockies.com',
+      division_name: "NL West"
+    },
+    {
+      name: 'Los Angeles Dodgers', 
+      stadium_name: 'Dodger Stadium', 
+      website: 'www.dodgers.com',
+      division_name: "NL West"
+    },
+    {
+      name: 'San Diego Padres', 
+      stadium_name: 'Petco Park', 
+      website: 'www.padres.com',
+      division_name: "NL West"
+    },
+    {
+      name: 'San Francisco Giants', 
+      stadium_name: 'Oracle Park', 
+      website: 'www.sfgiants.com',
+      division_name: "NL West"
+    }
+  ]
 }]
-
 
 module.exports = divisionsData
