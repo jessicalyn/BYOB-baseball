2d260313191676360cbcbf7a390c404467d43f26
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2019-05-03 09:45:44 -0600

    add set port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express()
 const port = 3000
 app.use(express.json())
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
-
+app.set('port', process.env.PORT || 3000)
 
 //Divisions endpoints
 
